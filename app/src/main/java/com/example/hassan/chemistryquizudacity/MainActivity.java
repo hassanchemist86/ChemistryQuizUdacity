64465a9705f61b9e0331526f70c76e0a8ed5b1d1
hassan <hassan_chemist86@yahoo.com>
2018-02-06 17:49:09 +0200

    update

--- app/src/main/java/com/example/hassan/chemistryquizudacity/MainActivity.java
+++ app/src/main/java/com/example/hassan/chemistryquizudacity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hassan.chemistryquizudacity;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaration of variables
     public Button results;
     RadioGroup rg_1, rg2, rg3, rg4;
     RadioButton Q1_radiobutton1, Q1_Radiobutton2, Q1_Radiobutton3, Q1_Radiobutton4;
@@ -51,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         Q5_ch3 = (CheckBox) findViewById(R.id.Q5_checkbox3);
     }
 
+
+    // calculation of results
     public int getScore(View view) {
 
         EditText username = findViewById(R.id.user_name);
@@ -64,50 +69,91 @@ public class MainActivity extends AppCompatActivity {
         RadioButton AnswerQ4 = findViewById(R.id.Q4_radio_button2);
         Boolean answerQ4 = AnswerQ4.isChecked();
         CheckBox AnswerQ5ch1 = findViewById(R.id.Q5_checkbox1);
-        boolean answerQ5 = AnswerQ5ch1.isChecked();
-        CheckBox AnswerQ5ch2 = findViewById(R.id.Q5_checkbox3);
+        boolean answerQ5ch1 = AnswerQ5ch1.isChecked();
+        CheckBox AnswerQ5ch2 = findViewById(R.id.Q5_checkbox2);
         boolean answerQ5ch2 = AnswerQ5ch2.isChecked();
+        CheckBox AnswerQ5ch3 = findViewById(R.id.Q5_checkbox3);
+        boolean answerQ5ch3 = AnswerQ5ch3.isChecked();
 
+        EditText Answer6 = findViewById(R.id.sixth) ;
+        String answer6 = Answer6.getText().toString () ;
+
+
+        //initializing the variable score and summation the right answers
         int score = 0;
         if (answerQ1) {
-            score = score + 2;
+            score = score + 3;
         }
         if (answerQ2) {
-            score = score + 2;
+            score = score + 3;
         }
         if (answerQ3) {
-            score = score + 2;
+            score = score + 3;
         }
         if (answerQ4) {
-            score = score + 2;
+            score = score + 3;
         }
-        if (answerQ5) {
+        if (answerQ5ch1) {
             score = score + 1;
         }
         if (answerQ5ch2) {
             score = score + 1;
         }
-        message = " Hi " + name + " your score is: " + score + " out of 10";
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
-        if (score >= 8) {
-            Toast.makeText(this, "Congratulations !! your grade is Excellent", Toast.LENGTH_LONG).show();
-        } else if (score >= 6)
-            Toast.makeText(this, "Great job !! your grade is GOOD", Toast.LENGTH_LONG).show();
-        else if (score >= 5) {
-            Toast.makeText(this, "well done !! your grade is pass", Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this, "You should study hard", Toast.LENGTH_LONG).show();
+        if (answerQ5ch3) {
+            score = score + 1;
         }
 
+        if (answer6.equals("neutral") || answer6.equals("Neutral")) {
+            score = score + 3;
+        }
+
+// Toast messages related to the answer
+        message = " Hi " + name + " your score is: " + score + " out of 18";
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        if (score >15) {
+            Toast.makeText(this, R.string.M1, Toast.LENGTH_SHORT).show();
+        } else if (score >13)
+            Toast.makeText(this, R.string.M2, Toast.LENGTH_SHORT).show();
+        else if (score > 10) {
+            Toast.makeText(this, R.string.M3, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, R.string.M4, Toast.LENGTH_SHORT).show();
+        }
         return score;
+    }
+
+// resetting method
+    public void RESET(View view) {
+
+        rg_1.clearCheck();
+        rg2.clearCheck();
+        rg3.clearCheck();
+        rg4.clearCheck();
+
+        if (Q5_ch1.isChecked()) {
+            Q5_ch1.setChecked(false);
+        }
+        if (Q5_ch2.isChecked()) {
+            Q5_ch2.setChecked(false);
+        }
+
+        if (Q5_ch3.isChecked()) {
+            Q5_ch3.setChecked(false);
+        }
+
+        //clear the user name field
+        EditText username = findViewById(R.id.user_name);
+        username.setText("");
+
+        // Clear the second EditText
+        EditText Answer6 = findViewById(R.id.sixth) ;
+        Answer6.setText("");
 
 
     }
-
-
 }
+
 
 
 
